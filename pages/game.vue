d22d36c3dd8e82caf84920d177dce111cdb3a2b7
Dimitri Guerroue <dimitri.guerroue@gmail.com>
2024-03-23 17:41:24 +0100

    wip: rules cricket closed scores + style

--- pages/game.vue
+++ pages/game.vue
@@ -1,39 +1,110 @@
 <template>
-    <div class="grid grid-cols-2 gap-4">
-        <div v-for="player in gameStore.game?.players" :key="player.id" class="flex size-full flex-col gap-2 py-6 dark:text-white">
-            <div class="mb-4 flex flex-col items-center justify-center border-2 py-4 dark:border-white">
-                <span>{{ player.name }}</span>
-                <span>{{ gameCricketStore.getScorePointsByPlayerId(player.id) }} pts</span>
+    <div class="flex size-full grow gap-4 py-6">
+        <!-- SCORE LIST -->
+        <div class="w-1/6">
+            <div class="flex size-full flex-col gap-2 dark:text-white">
+                <div class="mb-4 h-20"></div>
+                <div v-for="score in gameCricketStore.cricketScores" :key="score" class="flex grow items-center justify-center text-2xl font-bold dark:text-white">
+                    {{ score == 25 ? 'B' : score }}
+                </div>
             </div>
-            <!-- Start cricket grid -->
-            <div v-for="score in scores" :key="score" class="cricket_grid-item" @click="playerScore(player.id, score)">
-                {{ score }} <span class="text-sm text-red-400">{{ gameCricketStore.getScoreStateByPlayerId(player.id, score) }}</span>
+        </div>
+        <!-- PLAYERS SCORES -->
+        <div class="grid w-full grid-cols-2 gap-4">
+            <div v-for="player in gameStore.game?.players" :key="player.id" class="flex size-full flex-col gap-2 dark:text-white">
+                <div class="mb-4 flex h-20 flex-col items-center justify-center rounded-lg text-lg font-bold dark:bg-slate-800">
+                    <span class="capitalize">{{ player.name }}</span>
+                    <Transition name="blink" mode="out-in">
+                        <span :key="gameCricketStore.getScorePointsByPlayerId(player.id)">{{ gameCricketStore.getScorePointsByPlayerId(player.id) }}</span>
+                    </Transition>
+                </div>
+                <!-- Start cricket grid -->
+                <div v-for="score in gameCricketStore.cricketScores" :key="score" class="relative flex grow flex-col items-center justify-center" :class="{'pointer-events-none opacity-15': gameCricketStore.checkClosedScore(score)}"
+                     @click="playerScore(player.id, score)">
+                    <span v-if="gameCricketStore.getCountByScorePlayer(player.id, score) == 0" class="cross-step-0"></span>
+                    <span v-if="gameCricketStore.getCountByScorePlayer(player.id, score) >= 1" class="cross-step-1"></span>
+                    <span v-if="gameCricketStore.getCountByScorePlayer(player.id, score) >= 2" class="cross-step-2"></span>
+                    <span v-if="gameCricketStore.getCountByScorePlayer(player.id, score) >= 3" class="cross-step-3"></span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import type { CricketScore } from '~/stores/gameCricketStore';
-
 definePageMeta({
     layout: 'default',
     middleware: 'has-game'
 });
 
 const gameStore = useGameStore();
-
 const gameCricketStore = useGameCricketStore();
 
-const scores: CricketScore[] = ['bull', 20, 19, 18, 17, 16, 15];
+// function generateRandomArray(length: number, min: number = 1, max: number = 20): number[] {
+//     const randomArray: number[] = [];
+//     for (let i = 0; i < length; i++) {
+//         const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+//         randomArray.push(randomNumber);
+//     }
+//     return randomArray;
+// }
 
-function playerScore(id:number, score: CricketScore) {
+// gameCricketStore.setCricketScore([25, ...generateRandomArray(6)])
+
+function playerScore(id:number, score: number) {
     gameCricketStore.pushScore(id, score)
 }
 </script>
 
 <style lang="scss" scoped>
-.cricket_grid-item {
-    @apply flex grow flex-col items-center justify-center border-2 border-dashed py-4 dark:border-white text-lg font-bold uppercase active:scale-95 transition-transform;
+
+
+.cross-step-0,
+.cross-step-1,
+.cross-step-2,
+.cross-step-3 {
+    @apply absolute left-1/2 top-1/2 block rounded-lg bg-red-600 h-[6px] w-[70px] -translate-x-1/2 -translate-y-1/2;
+
+    &.cross-step-0 {
+        @apply w-[20px] bg-slate-200;
+    }
+    &.cross-step-1 {
+        @apply rotate-45 ;
+    }
+    &.cross-step-2 {
+        @apply -rotate-45;
+    }
+    &.cross-step-3 {
+        @apply size-[50px] bg-transparent border-red-600 border-[6px] rounded-full;
+    }
+}
+
+.cross-step-1,
+.cross-step-2,
+.cross-step-3 {
+    animation: fadeIn 0.2s ease-in-out;
+
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+}
+
+.blink-enter-active {
+  transition: all .2s ease;
+}
+
+.blink-leave-active {
+  transition: all .2s ease;
+}
+
+.blink-enter,
+.blink-leave-to {
+  transform: scale(1.5);
+  opacity: 0;
 }
 </style>
--- stores/gameCricketStore.ts
+++ stores/gameCricketStore.ts
@@ -1,13 +1,30 @@
 import { defineStore } from 'pinia'
+import { useGameStore } from './gameStore';
 
-export type CricketScore = 'bull' | 20 | 19 | 18 | 17 | 16 | 15;
 
 export const useGameCricketStore = defineStore('gameCricket', () => {
+    const gameStore = useGameStore();
+
+    const defaultCricketScores: [number, ...number[]] = [25, 20, 19, 18, 17, 16, 15];
+
+    const cricketScores = ref<[number, ...number[]]>(defaultCricketScores);
+
+    function setCricketScore(scores: [number, ...number[]]) {
+        cricketScores.value = scores;
+    }
+
+    // TODO: essayer de changer le type de score par ça:
+    type Score = {
+        value: number,
+        state: 1 | 2 | 3
+    }
+    
     const playersScores = ref<{
         playerId: number,
         scorePoints: number,
-        scoreOpen: Set<CricketScore>, // TODO: init this to empty a game start
-        playerScoreHistory: CricketScore[]
+        scoresOpen: number[],
+        scoresClose: number[],
+        playerScoreHistory: number[]
     }[]>([]);
 
     function getScoresByPlayerId(playerId: number) {
@@ -18,37 +35,56 @@ export const useGameCricketStore = defineStore('gameCricket', () => {
         return getScoresByPlayerId(playerId)?.scorePoints ?? 0
     }
 
-    function getScoreStateByPlayerId(playerId: number, score: CricketScore) {
+    function getCountByScorePlayer(playerId: number, score: number) {
         return getScoresByPlayerId(playerId)?.playerScoreHistory.filter(psh => psh == score).length ?? 0
     }
 
-    function pushScore(playerId: number, score: CricketScore) {
-        const playerScore = getScoresByPlayerId(playerId)
+    function checkClosedScore(score: number) {
+        const allScoresOpen: number[] = [];
+        const numPlayers = gameStore.game?.players.length ?? 0;
 
+        if(playersScores.value.length == 0) {
+            return;
+        }
+
+        playersScores.value.forEach(ps => { allScoresOpen.push(...ps.scoresOpen) });
+
+        const numberOfOpenScore = allScoresOpen.reduce((acc, value) => {
+            return value === score ? acc + 1 : acc;
+        }, 0);
+
+        return numberOfOpenScore >= numPlayers
+    }
+
+    function pushScore(playerId: number, score: number) {
+        const playerScore = getScoresByPlayerId(playerId);
+        
         if(playerScore === undefined) {
             playersScores.value.push({
                 playerId: playerId,
                 scorePoints: 0,
-                scoreOpen: new Set<CricketScore>(),
+                scoresOpen: [],
+                scoresClose: [],
                 playerScoreHistory: [score]
             })
         } else {
             playerScore.playerScoreHistory.push(score)
 
-            // ça score !
-            if(getScoreStateByPlayerId(playerId, score) >= 3) {
-                playerScore.scoreOpen.add(score);
+            if(checkClosedScore(score) == false) {
+        
+                // ça score !
+                if(getCountByScorePlayer(playerId, score) == 3) {
+                    playerScore.scoresOpen.push(score);
+                }
+                if(getCountByScorePlayer(playerId, score) > 3) {
+                    playerScore.scorePoints += score
+                }
             }
-            if(getScoreStateByPlayerId(playerId, score) > 3) {
-                playerScore.scorePoints += score === 'bull' ? 25 : score
-            }
-
-            console.log(playerScore)
         }
 
     }
 
-    return { playersScores, pushScore, getScoreStateByPlayerId, getScorePointsByPlayerId, getScoresByPlayerId }
+    return { cricketScores, setCricketScore, playersScores, pushScore, getCountByScorePlayer, checkClosedScore, getScorePointsByPlayerId, getScoresByPlayerId }
 }, {
     persist: {
         storage: persistedState.localStorage
